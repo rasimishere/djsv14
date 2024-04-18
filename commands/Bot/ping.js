a3a588e7ee3a2ac8f03e6a08045ff134c1b76e14
Rasim <90181726+rasimishere@users.noreply.github.com>
2024-04-18 06:54:54 +0200

    moving commands to the Bot folder

--- commands/Bot/ping.js
+++ commands/Bot/ping.js
@@ -1,1 +1,39 @@
+const { EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("ping"),
+
+        run: async (client, interaction) => {
+
+
+
+        const ping = client.ws.ping + "ms"
+
+        const embed = new EmbedBuilder()
+            .setColor('#5865F2')
+            .setTitle(`${client.user.username} Ping!`)
+            .setAuthor({ name: interaction.user.tag, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) })
+            .addFields(
+                {
+                    name: "Bot Delay",
+                    value: "```asciidoc\n= " + ping + " =\n```",
+                    inline: true,
+                },
+                {
+                    name: "Message Delay",
+                    value: "```asciidoc\n= " + Math.floor(new Date().getTime() - interaction.createdTimestamp) + "ms =\n```",
+                    inline: true,
+                },
+            )
+            .setFooter({ text: `${interaction.user.tag}.`, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) })
+            .setTimestamp()
+
+                interaction.reply({ embeds: [embed] });
+    }
+  
+}
 
