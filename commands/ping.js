a27661a785b3c0cce7cf13ab60740f6efa726623
Rasim <90181726+rasimishere@users.noreply.github.com>
2024-04-18 06:37:53 +0200

    using slashcommandbuilder

--- commands/ping.js
+++ commands/ping.js
@@ -1,2 +1,11 @@
 const { EmbedBuilder } = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("ping"),
+
+  
+}
