bc9ac3ebaf6759cc012829f418a7208dc96bdfa3
Rasim <90181726+rasimishere@users.noreply.github.com>
2024-04-18 06:01:51 +0200

    Defining manager variable for shards

--- shard.js
+++ shard.js
@@ -3,3 +3,6 @@ const chalk = new require('chalk');
 const { token, shard: totalShards, webhook: url } = require("./config.json");
 const kullanıcıAdı = "rrasim";
 let wb;
+
+
+const manager = new ShardingManager('bot.js', { token, respawn: true, totalShards });
