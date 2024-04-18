c3e1bda748bf37b5bb873c5e42cd3efe9c07f3b0
Rasim <90181726+rasimishere@users.noreply.github.com>
2024-04-18 05:35:56 +0200

    Set up events to load the commands and run the bot.

--- bot.js
+++ bot.js
@@ -14,5 +14,34 @@ client.commands = new Collection();
 client.slashcommands = new Collection();
 client.commandaliases = new Collection();
 
+const rest = new REST({ version: "10" }).setToken(token);
+
+const slashcommands = [];
+const commandFolders = fs.readdirSync('./src');
+
+console.log(chalk.red('[COMMANDS] Loading Commands...'));
+
+for (const folder of commandFolders) {
+    const commandFiles = fs.readdirSync(`./src/${folder}`).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const commandPath = `./src/${folder}/${file}`;
+        const command = require(commandPath);
+        const commandCategory = folder.toLocaleUpperCase();
+        slashcommands.push(command.data.toJSON());
+        client.slashcommands.set(command.data.name, command);
+        console.log(chalk.cyan('[KOMUT] ' + command.data.name + ' Yüklendi' + chalk.yellow(' - ') + chalk.red('Kategori: ' + commandCategory)));
+    }
+}
+client.on(Events.ClientReady, async () => {
+    try {
+        await rest.put(Routes.applicationCommands(client.user.id), {
+            body: slashcommands,
+        });
+    } catch (error) {
+        console.error(error);
+    }
+    console.log(chalk.red(`[SUCCESS]`) + chalk.green(` ${client.user.username} Aktif Edildi!`));
+});
+
 
 client.login(config.token);
