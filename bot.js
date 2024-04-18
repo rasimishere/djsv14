195d3a308d8303aac1aad8b312cf869f789ab4fb
Rasim <90181726+rasimishere@users.noreply.github.com>
2024-04-18 05:32:15 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,14 @@
+const { Client, GatewayIntentBits, Events, Collection} = require("discord.js");
+const fs = require('node:fs');
+const INTENTS = Object.values(GatewayIntentBits);
+const client = new Client({ intents: INTENTS });
+const config = require("./config.json");
+const chalk = require('chalk');
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v10");
+const { readdirSync } = require("fs");
+
+let token = config.token;
+
+
+client.login(config.token);
